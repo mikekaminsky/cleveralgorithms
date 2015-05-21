3f43ead57a85e6c6e1a2fb00f4a1bf9ccce457a7
mikekaminsky <mkaminsky@casecommons.org>
2015-05-21 12:28:13 -0400

    Starting work on visualizer. Learning d3

---
+++ visualizer/sandbox/data.js
@@ -0,0 +1,19 @@
+var JSONData = {"metadata":
+  {
+  "spacemin":-5,
+  "spacemax":5,
+  "solution":
+    {
+    "x":0, 
+    "y":0
+    },
+  "iterations":5
+},
+"data":[
+  {"iteration":1, "locations": [ {"x":4, "y":4 }, {"x":3, "y":1 }, {"x":3, "y":0 } ] },
+  {"iteration":2, "locations": [ {"x":4.2, "y":4.2 }, {"x":2.8, "y":1.5 }, {"x":2.1, "y":0.5 } ] },
+  {"iteration":3, "locations": [ {"x":4.4, "y":4.4 }, {"x":2.6, "y":1.5 }, {"x":2.3, "y":0.7 } ] },
+  {"iteration":4, "locations": [ {"x":4.6, "y":4.6 }, {"x":2.4, "y":1.5 }, {"x":2.5, "y":0.9 } ] },
+  {"iteration":5, "locations": [ {"x":4.8, "y":4.8 }, {"x":2.2, "y":1.5 }, {"x":2.7, "y":1.1 } ] }
+]
+}
